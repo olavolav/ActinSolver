95467890754819c80b518e73d132154f2b0024e6
Olav Stetter <olav.stetter@googlemail.com>
2013-03-20 11:24:12 +0100

    Fluor. simulator now with actual distributions.
    Gaussian for noise and poisson for the number of actin filaments at a given event.

--- ActinSimulator.py
+++ ActinSimulator.py
@@ -7,9 +7,10 @@ import math as math
 class ActinSimulator:
   """A simulator of actin fluorescence time series to test the Bayesian analysis."""
   
-  def __init__(self, a_rate, a_amplitude_mean, n_sigma, tau_k, tau_i, f_baseline=0.0):
+  def __init__(self, a_rate, avg_actin_c, fluoro_per_actin, n_sigma, tau_k, tau_i, f_baseline=0.0):
     self.rate = a_rate
-    self.amplitude_mean = a_amplitude_mean
+    self.actin_count_mean = avg_actin_c
+    self.fluorescence_per_actin = fluoro_per_actin
     self.noise_stddev = n_sigma
     self.tau_kernel = tau_k
     self.tau_image = tau_i
@@ -20,23 +21,25 @@ class ActinSimulator:
     signal_amplitudes = np.zeros_like(signal_times)
     
     # 1.) Generate (hidden) spike data
-    for i in np.arange(samples):
+    for i in range(samples):
       if random.random() < self.__spiking_probability():
+        print "DEBUG: simulating a spike at sample #{s} ({t}s).".format(s=i,t=(1.0*i)*self.tau_image)
         # there was a spike at that time point, now we have to determine its amplitude
-        signal_amplitudes[i] = self.amplitude_mean # <------------------------- for testing only!!!!!
+        # signal_amplitudes[i] = self.amplitude_mean # <------------------------- for testing only!!!!!
+        signal_amplitudes[i] = self.fluorescence_per_actin *np.random.poisson(self.actin_count_mean)
     
     # 2.) Convolve with kernel
     kernel = self.__generate_kernel()
     signal_amplitudes = np.convolve(signal_amplitudes, kernel, 'same')
     
-    # 3.) Add noise
+    # 3.) Add noise and baseline
     for i in np.arange(samples):
-      signal_amplitudes[i] += self.noise_stddev * random.random() # <---------- for testing only!!!!!
+      signal_amplitudes[i] +=  np.random.normal(0.0, self.noise_stddev) + self.baseline
     
     return [signal_times, signal_amplitudes]
   
   def generate_time_series_for_duration(self, time):
-    return self.generate_time_series( round(time / self.tau_image) )
+    return self.generate_time_series( int(round(time / self.tau_image)) )
   
   def __spiking_probability(self):
     return self.rate * self.tau_image
@@ -47,6 +50,5 @@ class ActinSimulator:
     for i in np.arange( kernel.size ):
       distance_in_s = (i-kernel_center_index) * self.tau_image
       kernel[i] = 1.0*math.exp( -abs(distance_in_s)/self.tau_kernel )
-    
     return kernel
   
--- test_simulation.py
+++ test_simulation.py
@@ -4,7 +4,7 @@ from ActinSimulator import *
 import matplotlib.pyplot as plt
 import numpy as np
 
-sim = ActinSimulator(3.0, 0.5, 0.5, 0.005, 0.001, 0.5)
+sim = ActinSimulator(2.0, 20, 0.1, 0.3, 0.005, 0.001, 0.5)
 signal_times, signal_amplitudes = sim.generate_time_series_for_duration(5.0)
 
 plt.xlabel('time (s)')
